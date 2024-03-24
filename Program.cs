732fd8e56277594e587077c09ee996dde79b8dd2
HMDSS <halilmertdeveliii@gmail.com>
2024-03-24 14:12:04 +0300

    delete and update is finished

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace project_for_six_week
     {
         static void Main(string[] args)
         {
+            AuthorContext authorContext = new AuthorContext();
+
             #region InstanceOfClass
             Author author = new Author
             {
@@ -46,26 +48,41 @@ namespace project_for_six_week
             #endregion
             #region AddOperation
 
-            AuthorContext authorContext = new AuthorContext();
 
-            try
-            {
-                CrudOperationAdd.AddNewAuthor(author, authorContext);//it is working
-                CrudOperationAdd.AddNewAuthorInfo(authorInfo, authorContext);
-                CrudOperationAdd.AddNewBook(book, authorContext);
-                authorContext.SaveChanges();
+            //try
+            //{
+            //    CrudOperationAdd.AddNewAuthor(author, authorContext);//it is working
+            //    CrudOperationAdd.AddNewAuthorInfo(authorInfo, authorContext);
+            //    CrudOperationAdd.AddNewBook(book, authorContext);
+            //    authorContext.SaveChanges();
 
-            }
-            catch
-            {
+            //}
+            //catch
+            //{
 
 
-            }
+            //}
 
-            Console.WriteLine("New records has been added");
+            //Console.WriteLine("New records has been added");
+            #endregion
+            #region DeleteOperaiton
+            CrudOperationDelete crudOperationDeleteInstance = new CrudOperationDelete();
+            //crudOperationDeleteInstance.DeleteFromAuthor(authorContext, 15, "GG");//working
+            //crudOperationDeleteInstance.DeleteFromBooks(authorContext, 13, "GG");//working
+            // crudOperationDeleteInstance.DeleteFromAuthorInfo(authorContext, 13, "F. Scott Fitzgerald was an American novelist...");
             #endregion
 
-            Console.WriteLine("1 -1 and 1 - to many example for week 6");
+            CrudOperationUpdate crudOperationUpdateInstance = new CrudOperationUpdate();
+            //crudOperationUpdateInstance.UpdateAuthor(authorContext, 1, "HMDSS", "Develi", "TR");//working
+            //crudOperationUpdateInstance.UpdateBook(authorContext, 1, "Irade Terbiyesi"); // working
+            //crudOperationUpdateInstance.UpdateAuthorInfo(authorContext, 1, "Yenikapı");//working
+
+            Console.WriteLine("1 -1 and 1 - to many example for week 6, Add operation is done" +
+                "Delete operation is done" +
+                "ss");
+
+
+
             Console.ReadLine();
         }
     }
@@ -159,5 +176,72 @@ public class CrudOperationAdd
 
         }
 
+    }
+}
+public class CrudOperationDelete
+{
+    public void DeleteFromAuthor(AuthorContext context, int entityId = 0, string stringEntityName = "")//this is for author
+    {
+        Author? authorForDelete = context.AuthorDbSet.FirstOrDefault(authorInstance => authorInstance.AuthorName == stringEntityName && authorInstance.AuthorId == entityId);
+        if (authorForDelete != null)
+        {
+            context.AuthorDbSet.Remove(authorForDelete);
+            context.SaveChanges();
+        }
+    }
+    public void DeleteFromBooks(AuthorContext context, int entityId = 0, string stringEntityName = "")//this is for books
+    {
+        Book? bookForDelete = context.BookDbSet.FirstOrDefault(bookInstance => bookInstance.BookID == entityId && bookInstance.BookName == stringEntityName);
+        if (bookForDelete != null)
+        {
+            context.BookDbSet.Remove(bookForDelete);
+            context.SaveChanges();
+        }
+    }
+    public void DeleteFromAuthorInfo(AuthorContext context, int entityId = 0, string stringEntityName = "")//this is for author
+    {
+        AuthorInfo? authorInfo = context.AuthorInfoDbSet.FirstOrDefault(authorInfoInstance => authorInfoInstance.AuthorId == entityId && authorInfoInstance.AuthorInformation == stringEntityName);
+        if (authorInfo != null)
+        {
+            context.AuthorInfoDbSet.Remove(authorInfo);
+            context.SaveChanges();
+        }
+    }
+
+}
+public class CrudOperationUpdate
+{
+    public void UpdateAuthor(AuthorContext context, int authorId = 0, string newAuthorName = "", string newAuthorLastName = "", string newAuthorNation = "")
+    {
+        Author? authorForUpdate = context.AuthorDbSet.FirstOrDefault(authorInstnce => authorInstnce.AuthorId == authorId);
+
+        if (authorForUpdate !=  null)
+        {
+            authorForUpdate.AuthorName = newAuthorName;
+            authorForUpdate.AuthorLastName = newAuthorLastName;
+            authorForUpdate.AuthorNationality = newAuthorNation;
+            context.Entry(authorForUpdate).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+    public void UpdateBook(AuthorContext context,int bookId=0,string newBookName = "")
+    {
+        Book? bookForUpdate = context.BookDbSet.FirstOrDefault(bookInstance => bookInstance.BookID == bookId);
+        if (bookForUpdate != null)
+        {
+            bookForUpdate.BookName = newBookName;
+            context.Entry(bookForUpdate).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+    public void UpdateAuthorInfo(AuthorContext context,int authorId=0,string newAuthorInfo="")
+    {
+        AuthorInfo? authorInfoForUpdate = context.AuthorInfoDbSet.FirstOrDefault(authorInfoDbSet => authorInfoDbSet.AuthorId == authorId);
+        if (authorInfoForUpdate != null)
+        {
+            authorInfoForUpdate.AuthorInformation = newAuthorInfo;
+            context.Entry(authorInfoForUpdate).State = EntityState.Modified;
+            context.SaveChanges();
+        }
     }
 }
